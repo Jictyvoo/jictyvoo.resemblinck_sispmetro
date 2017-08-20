3c6a50427b89353bee81a121ada7ffa0fb0b81e3
BlinckF <resemblinck@hotmail.com>
2017-08-20 13:59:37 -0300

    Revert "Merge branch 'master' of https://github.com/Jictyvoo/jictyvoo.resemblinck_sispmetro"
    This reverts commit b128b946fae6a48e28e3170d5ed02246e4b6b14a, reversing
    changes made to 8a52c026e982a9e3daadc4ed8bfc7567fd8c4a75.

--- sispmetro_sourceCode/src/br/uefs/ecomp/util/IStack.java
+++ sispmetro_sourceCode/src/br/uefs/ecomp/util/IStack.java
@@ -1,14 +1,15 @@
 package br.uefs.ecomp.util;
 
-public interface IStack<TypeReceived> {
+public interface IStack <TypeReceived> {
+	
 
 	public int size();
 
 	public boolean isEmpty();
-
+	
 	public TypeReceived pop();
-
+	
 	public void push(TypeReceived obj);
-
+	
 	public TypeReceived peek();
 }
